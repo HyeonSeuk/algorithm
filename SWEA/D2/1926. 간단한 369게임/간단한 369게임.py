b3d95df2df3eede65d7d6e30ec35702cbf925dfa
서현석 <seo5719@naver.com>
2023-01-23 21:26:05 +0900

    [D2] Title: 간단한 369게임, Time: 153 ms, Memory: 58,260 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -0,0 +1,9 @@
+a = int(input())
+for i in range(1, a+1):
+    i = str(i)
+    clap = i.count('3') + i.count('6') + i.count('9')
+    
+    if clap == 0:
+        print(i, end=' ')
+    else:
+        print('-'*clap, end=' ')
