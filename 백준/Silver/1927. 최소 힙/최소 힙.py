94fabbab34133dc474624598cf8b8b3454685aa6
서현석 <seo5719@naver.com>
2023-01-27 23:48:24 +0900

    [Silver II] Title: 최소 힙, Time: 124 ms, Memory: 36984 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,12 @@
+import heapq
+import sys
+heap = []
+for i in range(int(sys.stdin.readline())):
+    a = int(sys.stdin.readline())
+    if a != 0:
+        heapq.heappush(heap, a)
+    else:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
