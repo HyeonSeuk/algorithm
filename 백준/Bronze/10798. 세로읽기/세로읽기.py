c75ac4a4f57e0a124ed35977d16836f0110fd349
서현석 <seo5719@naver.com>
2023-01-31 14:31:34 +0900

    [Bronze I] Title: 세로읽기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,10 @@
+mat = []
+
+for _ in range(5):
+    line = list(input())
+    mat.append(line)
+  
+for i in range(max(len(a) for a in mat)):
+    for j in range(5):
+        if i < len(mat[j]):
+            print(mat[j][i], end='')
