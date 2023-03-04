988f0217804e654536568064fb2c9450bb316bcd
서현석 <seo5719@naver.com>
2023-03-04 22:42:12 +0900

    [Bronze II] Title: 너의 이름은 몇 점이니?, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/15813. 너의 이름은 몇 점이니？/너의 이름은 몇 점이니？.py
@@ -0,0 +1,57 @@
+n = int(input())
+lst = list(input())
+cnt = 0
+for i in lst:
+    if i == 'A':
+        cnt += 1
+    elif i == 'B':
+        cnt += 2
+    elif i == 'C':
+        cnt += 3
+    elif i == 'D':
+        cnt += 4
+    elif i == 'E':
+        cnt += 5
+    elif i == 'F':
+        cnt += 6
+    elif i == 'G':
+        cnt += 7
+    elif i == 'H':
+        cnt += 8
+    elif i == 'I':
+        cnt += 9
+    elif i == 'J':
+        cnt += 10
+    elif i == 'K':
+        cnt += 11
+    elif i == 'L':
+        cnt += 12
+    elif i == 'M':
+        cnt += 13
+    elif i == 'N':
+        cnt += 14
+    elif i == 'O':
+        cnt += 15
+    elif i == 'P':
+        cnt += 16
+    elif i == 'Q':
+        cnt += 17
+    elif i == 'R':
+        cnt += 18
+    elif i == 'S':
+        cnt += 19
+    elif i == 'T':
+        cnt += 20
+    elif i == 'U':
+        cnt += 21
+    elif i == 'V':
+        cnt += 22
+    elif i == 'W':
+        cnt += 23
+    elif i == 'X':
+        cnt += 24
+    elif i == 'Y':
+        cnt += 25
+    elif i == 'Z':
+        cnt += 26
+print(cnt)
