16a0a2d77c4d893477a45ad8cf6a82d65f600cf7
서현석 <seo5719@naver.com>
2023-03-26 14:54:06 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,10 +1,8 @@
 a, b = map(int, input().split())
 lst = []
-cnt = 0
 for i in range(1, a+1):
-    if a%i == 0:
+    if a % i == 0:
         lst.append(i)
-
 if len(lst) >= b:
     print(lst[b-1])
 else:
