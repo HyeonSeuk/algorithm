abd371d01f8628d4a0795d2bf9088236bc92ffe7
서현석 <seo5719@naver.com>
2023-04-03 22:05:09 +0900

    Update 소수.py

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -8,9 +8,13 @@ new_lst = []
 for j in lst:
     if j in [2, 3, 5, 7]:
         new_lst.append(j)
-    elif j > 1 and all(j % i != 0 for i in range(2, int(j**0.5)+1)):
-        new_lst.append(j)
-
+    elif j > 1:
+        for i in range(2, int(j**0.5)+1):
+            if j % i == 0:
+                break
+        else:  # for문이 정상적으로 종료된 경우
+            new_lst.append(j)
+            
 if not new_lst:
     print(-1)
 else:
