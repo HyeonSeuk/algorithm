a3638b5228f29a7dd7732938072075ec1eeec77d
서현석 <seo5719@naver.com>
2023-08-10 17:47:12 +0900

    [Bronze V] Title: X보다 작은 수, Time: 48 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,6 +1,6 @@
-a, b = list(map(int, input().split()))
-numbers = list(map(int, input().split()))
+a, b = list(map(int, input().split())) # 숫자 2개를 받는다
+numbers = list(map(int, input().split())) # 숫자들을 리스트로 넣는다
 
-for i in numbers:
-    if b > i:
-        print(i, end=' ')
+for i in numbers: # 반복문
+    if b > i: # 만약 조건 대로 숫자가 리스트의 숫자들이 작다면
+        print(i, end=' ') # 해당 순서대로 출력을 해라
