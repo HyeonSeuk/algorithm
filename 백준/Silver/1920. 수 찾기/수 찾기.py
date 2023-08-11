0f8460cfa7de0d0e5dc23dfcb63b0c149d5f96cd
서현석 <seo5719@naver.com>
2023-08-11 13:16:53 +0900

    Update 수 찾기.py

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,5 +1,5 @@
 a = int(input())
-lst = set(map(int, input().split()))
+lst = set(map(int, input().split())) #list --> O(n), set --> O(1) 시간 복잡도가 줄어든다?
 b = int(input())
 lit = list(map(int, input().split()))
 
